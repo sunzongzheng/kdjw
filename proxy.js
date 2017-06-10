e37082a400da938e5a4ddb87d4b880691f36eba8
sunzongzheng <sunzongzhengwy@163.com>
2017-06-10 12:44:24 +0800

    update proxy

--- proxy.js
+++ proxy.js
@@ -7,20 +7,20 @@ var express = require("express"),
 var app = express();
 var proxy = httpProxy.createServer();
 
-const site_url = "http://kdjw.dev.cc"
-
 http.createServer(app).listen(9039, '0.0.0.0');
 
-app.use('/kdjw', function (req, res) {
-    res.header("Access-Control-Allow-Origin", site_url);
+app.all('*', (req, res, next) => {
+    res.header("Access-Control-Allow-Origin", req.headers.referer.substr(0, req.headers.referer.length - 1));
     res.header("Access-Control-Allow-Credentials", true);
+    next();
+})
+
+app.use('/kdjw', function (req, res) {
     proxy.web(req, res, {
         target: 'http://kdjw.hnust.cn/kdjw'
     })
 });
 app.use('/xxjw', function (req, res) {
-    res.header("Access-Control-Allow-Origin", site_url);
-    res.header("Access-Control-Allow-Credentials", true);
     proxy.web(req, res, {
         target: 'http://xxjw.hnust.cn/xxjw'
     })
